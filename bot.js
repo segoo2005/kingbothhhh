11297355bf068fa0c5349997f21b789c829f9e2d
segoo2005 <43680429+segoo2005@users.noreply.github.com>
2019-02-03 21:07:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,3 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-
-client.login('token');
-
-
-
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "&";
