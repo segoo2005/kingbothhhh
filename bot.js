9ec110bf466ce99c22fd3628c989db069bf7e5bc
segoo2005 <43680429+segoo2005@users.noreply.github.com>
2019-04-13 00:03:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+console.log("Scrpit By Dream");
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-
-client.login('token');
-
+client.on("ready", () => {
+let channel =     client.channels.get("566377272366596096")
+setInterval(function() {
+channel.send(`youtube`);
+}, 25)
+})
+ 
+ 
+client.login('BOT_TOKEN');
