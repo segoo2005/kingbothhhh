ccfb38f17051bffe772d9f61e22794c29cbe8d66
segoo2005 <43680429+segoo2005@users.noreply.github.com>
2019-04-13 09:37:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("هنا اي دي الروم")
+let channel =     client.channels.get("566377324807979089")
 setInterval(function() {
-channel.send(`هنا كلام ال سبام`);
+channel.send(`بسم الله الرحمن الرحيم وهنبدا الليله`);
 }, 30)
 })
 
